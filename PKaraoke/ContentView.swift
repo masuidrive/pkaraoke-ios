047da7ec995fc769b1d0fe9c9b52e94c537f77fd
Yuichiro MASUI <masui@masuidrive.jp>
2019-07-27 16:32:28 +0900

    Initial Commit

---
+++ PKaraoke/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  PKaraoke
+//
+//  Created by Yuichiro MASUI on 2019/07/27.
+//  Copyright © 2019 Yuichiro MASUI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
